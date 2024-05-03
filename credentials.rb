ea798677181c3528c92233165e36672b1b6073a7
Julio Cabrera <juliocabrera820@gmail.com>
2024-05-03 02:33:20 -0600

    Ft/publish gem (#3)
    * module to provider action inputs
    
    * add module to generate credentials
    
    * add module to publish gem in gh packs and rubygems
    
    * avoid duplicate validation
    
    * add steps to build and publish gem
    
    * fix: remove file if exists

--- credentials.rb
+++ credentials.rb
@@ -1,14 +1,37 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
+require_relative 'inputs'
+
 # Module to handle credentials
 module Credentials
-  RUBYGEMS_API_KEY = ENV['RUBYGEMS_API_KEY']
-  GITHUB_TOKEN = ENV['GITHUB_TOKEN']
-  GITHUB_USERNAME = ENV['GITHUB_USERNAME']
+  def self.generate_gh_credentials
+    credentials = <<~CREDENTIALS
+      ---
+      :github: Bearer #{Inputs::GITHUB_TOKEN}
+    CREDENTIALS
+    write_credentials(credentials)
+  end
 
-  def self.print_credentials
-    puts "RUBYGEMS_API_KEY: #{RUBYGEMS_API_KEY}"
-    puts "GITHUB_TOKEN: #{GITHUB_TOKEN}"
-    puts "GITHUB_USERNAME: #{GITHUB_USERNAME}"
+  def self.generate_rubygems_credentials
+    credentials = <<~CREDENTIALS
+      ---
+      :rubygems_api_key: #{Inputs::RUBYGEMS_API_KEY}
+    CREDENTIALS
+    write_credentials(credentials)
+  end
+
+  def self.write_credentials(credentials)
+    credentials_dir_path = "#{Dir.home}/.gem"
+    FileUtils.mkdir_p(credentials_dir_path)
+    credentials_file_path = "#{credentials_dir_path}/credentials"
+    File.open(credentials_file_path, 'w') { |f| f.write(credentials) }
+    FileUtils.chmod(0o600, credentials_file_path)
+  end
+
+  def self.delete_credentials_file_path
+    credentials_file_path = "#{Dir.home}/.gem/credentials"
+    FileUtils.rm(credentials_file_path) if File.exist?(credentials_file_path)
   end
 end
--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 require_relative 'gem_builder'
+require_relative 'publish'
+require_relative 'credentials'
 
-GemBuilder.build
+begin
+  GemBuilder.build
+  Publish.to_rubygems unless Inputs::RUBYGEMS_API_KEY.nil?
+  Publish.to_github_packages unless Inputs::GITHUB_TOKEN.nil?
+ensure
+  Credentials.delete_credentials_file_path
+end
