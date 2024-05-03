cca14dde4e08625e9ce19150799b7129ae0d83a5
juliocabrera820 <juliocabrera820@gmail.com>
2024-05-03 02:11:10 -0600

    module to provider action inputs

---
+++ inputs.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Module to handle action inputs
+module Inputs
+  RUBYGEMS_API_KEY = ENV['RUBYGEMS_API_KEY']
+  GITHUB_TOKEN = ENV['GITHUB_TOKEN']
+  GITHUB_USERNAME = ENV['GITHUB_USERNAME']
+end
