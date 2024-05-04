9ae3b4afa2d3835618192c96a9179a3fb61a99a3
Julio Cabrera <juliocabrera820@gmail.com>
2024-05-04 14:03:34 -0600

    avoid overwriting credentials (#8)
    * generated credentials
    
    * rename file
    
    * add methods to handle action inputs
    
    * fix: avoid overwriting user credentials
    
    * fix: set correct action

--- inputs.rb
+++ inputs.rb
@@ -5,4 +5,24 @@ module Inputs
   RUBYGEMS_API_KEY = ENV['RUBYGEMS_API_KEY']
   GITHUB_TOKEN = ENV['GITHUB_TOKEN']
   GITHUB_USERNAME = ENV['GITHUB_USERNAME']
+
+  def self.github_token?
+    !GITHUB_TOKEN.nil?
+  end
+
+  def self.rubygems_api_key?
+    !RUBYGEMS_API_KEY.nil?
+  end
+
+  def self.github_token
+    GITHUB_TOKEN
+  end
+
+  def self.rubygems_api_key
+    RUBYGEMS_API_KEY
+  end
+
+  def self.github_username
+    GITHUB_USERNAME
+  end
 end
--- main.rb
+++ main.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-require_relative 'gem_builder'
-require_relative 'publish'
 require_relative 'credentials'
+require_relative 'gem_builder'
+require_relative 'inputs'
+require_relative 'publish'
 
 begin
   GemBuilder.build
-  Publish.to_rubygems unless Inputs::RUBYGEMS_API_KEY.nil?
-  Publish.to_github_packages unless Inputs::GITHUB_TOKEN.nil?
+  Credentials.generate_credentials_file
+  Publish.to_rubygems if Inputs.rubygems_api_key?
+  Publish.to_github_packages if Inputs.github_token?
 ensure
   Credentials.delete_credentials_file_path
 end
